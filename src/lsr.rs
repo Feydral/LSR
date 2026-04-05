7af56cd193d0e4090811879c6c04f4dfdb03351e
Feydral <lian.hable.feathertech@gmail.com>
2026-04-05 09:33:13 +0200

    feat(rasterizer): create `PrimitiveMode`

--- src/lsr.rs
+++ src/lsr.rs
@@ -2,6 +2,14 @@ pub mod shader;
 pub mod render_target;
 
 use crate::lsr::shader::{FragmentShader, VertexShader};
+use crate::lsr::render_target::RenderTarget;
+use crate::math::numerics::{float2::Float2, float3::Float3};
+
+pub enum PrimitiveMode {
+    Triangles,
+    Lines,
+    Points,
+}
 
 pub fn draw_arrays(target: &mut RenderTarget, mode: PrimitiveMode, vertices: &[Float3], uvs: &[Float2], normals: &[Float3], vshader: &impl VertexShader, fshader: &impl FragmentShader) {
 
